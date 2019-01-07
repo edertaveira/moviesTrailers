b7d650fc44d7973514eacc2d3f05f627e8f14a07
Eder Taveira <edertaveira@Eders-MacBook-Pro.local>
2019-01-07 13:41:10 -0200

    Corrections about the revision.

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
 import webbrowser
 
-class Movie() :
-    def __init__(self, movie_title, movie_storyline, poster_url, trailer_youtube):
+
+class Movie():
+    def __init__(self, movie_title, movie_storyline,
+                 poster_url, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_url
         self.trailer_youtube_url = trailer_youtube
 
+    # Open the Trailer in a box
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
